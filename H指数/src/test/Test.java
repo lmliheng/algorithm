cba0c1d4563f8a276b97653500ee6a9d1026c487
lmliheng <liheng2137@126.com>
2024-09-09 15:59:47 +0800

    add :完善忽略文件

--- H指数/src/test/Test.java
+++ H指数/src/test/Test.java
@@ -1,4 +1,12 @@
 package H指数.src.test;
 
+import H指数.src.main.Solution;
+
 public class Test {
+    public static void main(String[] args) {
+        int[] citations = {3, 0, 6, 1, 5};
+        Solution solution = new Solution();
+        int hIndex = solution.hIndex(citations);
+        System.out.println("H-index: " + hIndex);
+    }
 }
