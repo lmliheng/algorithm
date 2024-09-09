0ec79ea41a2a4eedd63640f8ba53a8e3cd17a132
lmliheng <liheng2137@126.com>
2024-09-09 23:21:10 +0800

    add :三数之和

--- 三数之和/src/main/Solution.java
+++ 三数之和/src/main/Solution.java
@@ -1,5 +1,19 @@
-//class Solution {
-//    public List<List<Integer>> threeSum(int[] nums) {
-//
-//    }
-//}
+package 三数之和.src.main;
+
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        boolean b = nums.length == 3 && nums[0] == nums[1] && nums[0] == nums[2];
+
+        if (nums.length < 3)         return null;
+
+
+
+
+
+
+
+
+    }
+}
