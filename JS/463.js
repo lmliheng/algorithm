39497a83825c3fb94dba38170300a1503f2ef8d5
lmliheng <liheng2137@126.com>
2026-05-29 11:50:03 +0800

    feat: 岛屿周长

---
+++ JS/463.js
@@ -0,0 +1,18 @@
+let grid = [
+    [0, 1, 0, 0],
+    [1, 1, 1, 0],
+    [0, 1, 0, 0],
+    [1, 1, 0, 0]]
+let res = 0
+for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] === 1) {
+            console.log(i, j, 'res=', res)
+            res += 4
+            if (i > 0 && grid[i - 1][j] === 1) { res -= 2 }
+            if (j > 0 && grid[i][j - 1] === 1) { res -= 2 }
+        }
+    
+    }
+}
+console.log(res)
