02bbd05086a86c21d20215d247b3794c73acefd4
liheng <liheng2137@126.com>
2026-06-15 22:29:23 +0800

    feat: (num>>>0).toString(2)

---
+++ 基本算法/十进制转二进制/toTwo.js
@@ -0,0 +1,8 @@
+let num = 1226
+
+console.log(num.toString(2))// 10011001010
+
+// 负数处理
+let num_1 = -1226
+console.log(num_1.toString(2)) // -10011001010
+console.log((num_1 >>> 0).toString(2))// 11111111111111111111101100110110
