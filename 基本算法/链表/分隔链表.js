0f26005a945935496578d3bcab89bb7e6ea2d7f1
liheng <liheng2137@126.com>
2026-06-16 12:07:38 +0800

    feat: 分隔链表

---
+++ 基本算法/链表/分隔链表.js
@@ -0,0 +1,26 @@
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+let head = new ListNode(0)
+
+var partition = function (head, x) {
+    let small = new ListNode(0);
+    const smallHead = small;
+    let large = new ListNode(0);
+    const largeHead = large;
+    while (head !== null) {
+        if (head.val < x) {
+            small.next = head;
+            small = small.next;
+        } else {
+            large.next = head;
+            large = large.next;
+        }
+        head = head.next;
+    }
+    large.next = null;
+    small.next = largeHead.next;
+    return smallHead.next;
+};
