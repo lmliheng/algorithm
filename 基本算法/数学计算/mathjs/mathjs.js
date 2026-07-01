a08ee6cf6fa9ba7bd98b844c6f3e9427ae605e28
liheng <liheng2137@126.com>
2026-07-01 18:41:52 +0800

    feat: mathjs

--- 基本算法/数学计算/mathjs/mathjs.js
+++ 基本算法/数学计算/mathjs/mathjs.js
@@ -1,3 +1,11 @@
 /**
  * @mathjs
  */
+
+import { eigs, matrix } from 'mathjs';
+
+const A = [[1, 2], [3, 4]];
+const { values, vectors } = eigs(A);
+
+console.log('特征值:', values.toArray ? values.toArray() : values);
+console.log('特征向量(列):', vectors.toArray ? vectors.toArray() : vectors);
