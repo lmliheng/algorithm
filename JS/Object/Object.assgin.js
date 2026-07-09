cee4e988de36505325690871b2be1d875fe22128
lmliheng <liheng2137@126.com>
2026-07-09 21:16:03 +0800

    feat: 合并对象属性

--- JS/Object/Object.assgin.js
+++ JS/Object/Object.assgin.js
@@ -12,3 +12,24 @@ let b = {
     name: '里斯'
 }
 console.log(Object.assign(a, b))
+
+
+/***
+ * 根据id合并数组lc
+ */
+let arr1 = [
+    { "id": 1, "b": { "b": 94 }, "v": [4, 3], "y": 48 }
+]
+let arr2 = [
+    { "id": 1, "b": { "c": 84 }, "v": [1, 3] }
+]
+
+let res = {}
+for (let obj of arr1) {
+// ?? 是什么写法 空值合并运算符
+    res[obj.id] = Object.assign(res[obj.id] ?? {}, obj)
+}
+for (let obj of arr2) {
+    res[obj.id] = Object.assign(res[obj.id] ?? {}, obj)
+}
+console.log(Object.values(res).sort((a, b) => a.id - b.id))
--- 基本算法/并查集/图的路径存在性查询.js
+++ 基本算法/并查集/图的路径存在性查询.js
@@ -2,6 +2,7 @@
 import { UnionFind } from './union.js'
 /**
  * @图的路径存在性查询
+ * @超时了... 可以剪枝
  * 
  */
 let n = 4, nums = [2, 5, 6, 8]
@@ -12,10 +13,14 @@ let res = []
 let len = queries.length
 let uf = new UnionFind(n)
 
+nums.sort((a, b) => a - b)
+
 for (let i = 0; i < len; i++) {
     for (let j = i; j < len; j++) {
         if (Math.abs(nums[i] - nums[j]) <= maxDiff) {
             uf.union(i, j)
+        } else {
+            break // break会跳出循环 还是直接废除该for循环
         }
     }
 }
