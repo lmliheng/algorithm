07f65a978a1ee597affe160e08349a2e6e82e891
lmliheng <liheng2137@126.com>
2026-07-12 11:47:11 +0800

    feat: 回溯

---
+++ 基本算法/回溯/组合总和.js
@@ -0,0 +1,25 @@
+/**
+ * @组合总和
+ * path没有写在回溯函数参数内
+ */
+let candidates = [2, 3, 6, 7]
+let target = 7
+let n = candidates.length
+let res = []
+let path = []
+const trackback = (start, sum) => {
+    if (sum === target) {
+        res.push([...path])
+        return
+    }
+    if (sum > target) {
+        return
+    }
+    for (let i = start; i < n; i++) {
+        path.push(candidates[i])
+        trackback(i, candidates[i] + sum)
+        path.pop()
+    }
+}
+trackback(0, 0)
+console.log(res)
