78158432195fe19a3511b27ef7196b83f3d23c0f
lmliheng <liheng2137@126.com>
2026-07-14 19:43:01 +0800

    feat: parseInt

---
+++ 基本算法/进制/toTen.js
@@ -0,0 +1,9 @@
+/**
+ * @n进制转10
+ */
+let a = '1010111'
+console.log(parseInt(a, 2)) // 87
+console.log(parseInt(a, 3)) // 823
+
+console.log(parseInt(a, 2).toString(2)) // a
+console.log(parseInt(a, 3).toString(3)) // a
