6b5e88800c4a6e8ce7d7cd8898e50c5145bc482f
lmliheng <liheng2137@126.com>
2026-07-28 23:47:01 +0800

    feat: 广度优先搜索之队列元素使用对象，和DFS多一个形参记录路径等信息一个道理

--- 基本算法/广度优先遍历/单词接龙.js
+++ 基本算法/广度优先遍历/单词接龙.js
@@ -1,50 +1,90 @@
 /**
  * @单词接龙
+ * @在BFS的队列定义中使用了对象作为元素记录step
  */
 let beginWord = "hit"
 let endWord = "cog"
 let wordList = ["hot", "dot", "dog", "lot", "log", "cog"]
 
+let n = wordList.length
 
-function isOneWordDiff(s1,s2){
+function isOneWordDiff(s1, s2) {
     // s1.length===s2.length
-    let n=s1.length
-    let diffNum=0
-    for(let i=0;i<n;i++){
-        if(s1[i]!==s2[i]){
+    let n = s1.length
+    let diffNum = 0
+    for (let i = 0; i < n; i++) {
+        if (s1[i] !== s2[i]) {
             diffNum++
         }
 
-        if(diffNum>1){
+        if (diffNum > 1) {
             return false
         }
     }
-    if(diffNum===1){
+    if (diffNum === 1) {
         return true
-    }else{
+    } else {
         return false
     }
 }
-//create graph 开销很大
+
+// console.log(isOneWordDiff('sssw', 'wssw'))
+//create graph 开销很大-无向图生成
 /**
  * {
  * 'hit':['hot'],
  * 'hot':['dot','lot'],
- * 'dot':['dog','lot'],
- * 'dog':['log','cog'],
- * 'lot':['hot','dot'],
- * 'log':['dog','cog'],
+ * 'dot':['dog','lot','hot'],
+ * 'dog':['log','cog','dot'],
+ * 'lot':['hot','dot','log'],
+ * 'log':['dog','cog','lot'],
  * 'cog':['dog','cog']
  * 
  * }
  */
 
-let graph={}
-for(let i=0;i<wordList.length;i++){
-
+let graph = {}
+// 初始化beginWord
+for (let i = 0; i < n; i++) {
+    if (isOneWordDiff(beginWord, wordList[i])) {
+        if (graph[beginWord] === undefined) {
+            graph[beginWord] = [wordList[i]]
+        } else {
+            graph[beginWord].push(wordList[i])
+        }
+    }
+}
+for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+        if (wordList[i] === wordList[j]) {
+            continue
+        }
+        if (isOneWordDiff(wordList[i], wordList[j])) {
+            if (graph[wordList[i]] === undefined) {
+                graph[wordList[i]] = [wordList[j]]
+            } else {
+                graph[wordList[i]].push(wordList[j])
+            }
+        }
+    }
 }
 
-let stack=[]
+console.log(graph)
+let visit = new Set()
+let quene = [{ word: beginWord, step: 1 }]
+visit.add(beginWord)
+while (quene.length) {
+    let { word, step } = quene.shift()
+    if (word === endWord) {
+        console.log(step)
+    }
+    let neighbors = graph[word] || []
+    for (let w of neighbors) {
+        if (!visit.has(w)) {
+            visit.add(w)
+            quene.push({ word: w, step: step + 1 })
+        }
+    }
+}
 
-
-console.log(isOneWordDiff('sssw','wssw'))
+// console.log(0)
