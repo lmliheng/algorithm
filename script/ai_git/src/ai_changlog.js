49d9a6b18c78066c88d3e7e1539c9964d01a4d6c
lmliheng <liheng2137@126.com>
2026-08-15 20:02:52 +0800

    feat: git ai

--- script/ai_git/src/ai_changlog.js
+++ script/ai_git/src/ai_changlog.js
@@ -8,14 +8,22 @@ import { messageAdd, messageCreate } from '../../../JS/AIModelCallMethod/deepsee
  * 对changlog进行整理，可以不使用
  * 
  */
-export async function ai_changlog(logPath) {
+export async function ai_changlog(
+  logPath,
+  options = {
+    branch: 'master',
+  }) {
+
+  if (logPath === undefined) {
+    console.error('请写入日志文件绝对地址')
+  }
   try {
     let FileContent = await readFile(logPath)
     let res = await deepseek_response(messageCreate(`帮我整理内容，整体格式和内容不要变，主要看看有没有多余的行或者行分布不均的情况，${FileContent}`))
     await writeFile(logPath, res, { flag: 'w', encoding: 'utf8' })
     console.log(res)
-  }catch(e){
+  } catch (e) {
     console.error(e)
   }
- 
+
 }
--- script/ai_git/src/ai_comsume.js
+++ script/ai_git/src/ai_comsume.js
@@ -4,8 +4,6 @@ import { exec } from 'child_process'
 import path from 'path'
 import { writeFile } from 'fs/promises'
 
-let git_commit = ''
-
 /**
  * @Ai总结commits内容
  * 这个要在commit后执行  对应钩子是 post-commit
@@ -17,26 +15,39 @@ let git_commit = ''
  * 形参路径是changlog的绝对路径
  * 
  */
-export async function ai_commsume(logPath) {
+export async function ai_commsume(
+    logPath,
+    options = {
+        branch: 'master',
+    }
+) {
+
+
+    if (logPath === undefined) {
+        console.error('请写入日志文件绝对地址')
+        return
+    }
+
     try {
-        const git_commit = await getGitCommit();
-        // console.log(git_commit);
-        // if (process.argv[2] === '--write') {
+        const git_commit = await getGitCommit(options);
+        if (git_commit.length == 0) {
+            console.error('最近没有提交内容')
+            return
+        }
         let question = messageCreate(`用md格式帮我总结我每次提交的内容 一个commit对应150字总结，标题是commit的内容和时间，说说我做了哪些工作，可以扩展什么，工作质量怎么样,${git_commit}`)
         let res = await deepseek_response(question)
         console.log('回复：', res)
         await writeFile(logPath, '\n' + res, { flag: 'a', encoding: 'utf8' });
-        // }
 
     } catch (error) {
         console.error('获取提交信息失败:', error.message);
     }
 }
 
-function getGitCommit() {
+function getGitCommit(options) {
     return new Promise((resolve, reject) => {
         // git show HEAD
-        exec('git log origin/master..HEAD -p', (error, stdout, stderr) => {
+        exec(`git log origin/${options?.branch}..HEAD -p`, (error, stdout, stderr) => {
             if (error) {
                 reject(error);
                 return;
